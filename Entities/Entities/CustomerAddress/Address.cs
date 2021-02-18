af072ed1061b8ab754481c009107f65d56808bcc
RedPlus <redplus@redplus.net>
2021-02-18 09:07:22 +0900

    Customer, Address

---
+++ Entities/Entities/CustomerAddress/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.CustomerAddress
+{
+    public class Address
+    {
+        public string Line { get; set; }
+        public string PostalOrZipCode { get; set; }
+        public string StateOrProvince { get; set; }
+        public string CityOrTown { get; internal set; }
+    }
+}
---
+++ Entities/Entities/CustomerAddress/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.CustomerAddress
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public Address WorkAddress { get; set; }
+    }
+}
