66e8240e95f78d2555968c0f7846fd922023e2d4
Park YongJune <redplus@redplus.net>
2022-07-20 02:52:14 +0900

    Todo Model Class

---
+++ Entities/Entities/Todos/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities.Todos
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        //[Required]
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
