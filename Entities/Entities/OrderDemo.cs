3b0e604604591d3024f8d0792785bad14e39383f
Park YongJune <redplus@redplus.net>
2025-03-22 14:51:45 +0900

    .

---
+++ Entities/Entities/OrderDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    internal class OrderDemo
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset OrderDate { get; set; }
+
+        public string OrderNumber { get; set; }
+
+        public string Status { get; set; }
+
+        public long SalesId { get; set; }
+    }
+}
