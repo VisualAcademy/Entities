5f68ac9778e3360f422190444efc1d5ce1c2101f
Park YongJune <redplus@redplus.net>
2025-03-27 17:57:28 +0900

    .

--- Entities/Entities/CustomerOrderOrderDetails/Customer.cs
+++ Entities/Entities/CustomerOrderOrderDetails/Customer.cs
@@ -6,5 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         //public string? MiddleName { get; set; }
+
+        public int Age { get; set; }
     }
 }
